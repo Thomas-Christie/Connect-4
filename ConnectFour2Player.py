ee69844dd61817d2c73459499e57847752b3f265
Thomas-Christie <thomashamish@hotmail.com>
2018-08-31 15:51:44 +0100

    Update ConnectFour2Player.py

--- ConnectFour2Player.py
+++ ConnectFour2Player.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     # Initialise the game engine
     # Modify these parameters to tweak the game
     app = ConnectFourEngine.ConnectFour(
-            red_player = ConnectFourAlphaBeta.AIcheck,
-            blue_player = ConnectFourMinimaxAI.AIcheck,
+            red_player = None,
+            blue_player = None,
             )
         # start the game engine
     app.game_loop()
